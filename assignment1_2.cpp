b57ca3bd56cc74a1664b3d8bf8aea52f355dac57
guptaaniket261 <guptaaniket261@gmail.com>
2021-03-08 19:26:21 +0530

    both operations performed

--- assignment1_2.cpp
+++ assignment1_2.cpp
@@ -4,29 +4,41 @@
 #include <opencv2/video/background_segm.hpp>
 #include "opencv2/highgui/highgui.hpp"
 
+
 using namespace cv;
 using namespace std;
 
-vector<long double> aa;
 
-void get_frac(Mat frame){
-  
+vector<long double> aa;     //for queue density
+vector<long double> bb;     //for dynamic density
+
+
+// function to give the fraction of road occupied
+
+void get_frac(Mat frame, int flag){
+
   long double cc=0;
   for(int i=0;i<frame.rows;i++){
     for(int j=0;j<frame.cols;j++){
       Vec3b bgrPixel = frame.at<Vec3b>(i,j);
+      // pixel values
       cc+=bgrPixel.val[0];
       cc+=bgrPixel.val[1];
       cc+=bgrPixel.val[2];
-    }
+    
   }
-  aa.push_back(cc);
+  }
+
+  if(flag==0)aa.push_back(cc);  //for queue density
+  else bb.push_back(cc);	 // for dynamic density
 }
+
+
 
 
 int main(int argc, char* argv[])
 {
-
+  //for helping in case of invalid command
   if (argc==1){
       cout << "You need to pass both ./part2 and <video_file> name as parameters\n";
       return 0;
@@ -35,32 +47,38 @@ int main(int argc, char* argv[])
       cout <<"Only 1 video file can be processed at a time.\n";
       return 0;
   }
-  
-  VideoCapture cap(argv[1]); 
+
+
+  VideoCapture cap(argv[1]);   //video filename is given as argument
+
   if (cap.isOpened() == false)  
   {
     cout << "Cannot open the video file" << endl;
     cin.get(); 
     return -1;
   }
-  
-  Mat bg;
+
+
+  Mat bg;   //background image
   cap.read(bg);
-  Mat bg_changed;
+  Mat bg_changed; //grayscale background image
   cvtColor(bg,bg_changed,COLOR_BGR2GRAY);
 
-  String window_name = "My First Video";
-  String window_name2 = "Binary video";
+  String window_name = "Traffic_Video";  //window names
+
+  //String window_name2 = "Binary video";
 
   namedWindow(window_name, WINDOW_NORMAL);
-  namedWindow(window_name2, WINDOW_NORMAL);
+  //namedWindow(window_name2, WINDOW_NORMAL);
 
+  
+  //coordinates for finding the homography matrix for angle correction of each frame
   int x0=472, y0=52, x1=472, y1=830, x2=800, y2=830, x3=800, y3=52;        
   vector<Point2f> a={Point2f(x0,y0),Point2f(x1,y1),Point2f(x2,y2),Point2f(x3,y3)};
   vector<Point2f> b={Point2f(984,204),Point2f(264,1068),Point2f(1542,1068),Point2f(1266,222)};
 
-  Mat H=findHomography(b,a);
-  warpPerspective(bg_changed,bg_changed,H,bg_changed.size());
+  Mat H=findHomography(b,a);   //homography matrix
+  warpPerspective(bg_changed,bg_changed,H,bg_changed.size());  //angle corrected
 
   Rect region;                      //region shows the section of window which is to be cropped
   region.x=a[0].x;
@@ -68,13 +86,11 @@ int main(int argc, char* argv[])
   region.width=a[2].x-a[0].x;
   region.height=a[2].y-a[0].y;
 
-  bg_changed = bg_changed(region);
-
-  Mat prvs;
+  bg_changed = bg_changed(region); //cropped background
+  Mat prvs;         //previous frame for optical flow
   bg_changed.copyTo(prvs);
-  int count=0;
 
-
+  int count=0;    //counter used to set fps processed
 
   while (true)
   {
@@ -85,29 +101,33 @@ int main(int argc, char* argv[])
       cout << "Found the end of the video" << endl;
       break;
     }
-    count++;
-    if (count%5!=0) continue;
+
+    count++;   
+
+    if (count%5!=0) continue; //setting fps processed using counter
     
     Mat gray_frame;
-    cvtColor(frame,gray_frame,COLOR_BGR2GRAY);
+    cvtColor(frame,gray_frame,COLOR_BGR2GRAY); //grayscale current frame
     //show the frame in the created window
+
     imshow(window_name, frame);
-    warpPerspective(gray_frame,gray_frame,H,gray_frame.size());
-    gray_frame = gray_frame(region);
+    warpPerspective(gray_frame,gray_frame,H,gray_frame.size()); //angle corrected current frame
+    gray_frame = gray_frame(region);  //cropped current frame
 
     Mat img3;
-    absdiff(gray_frame,bg_changed,img3);
-      
+    absdiff(gray_frame,bg_changed,img3);  //after removing background
+
     Mat img3_binary;
-    threshold(img3, img3_binary, 25, 255, THRESH_BINARY);
+    threshold(img3, img3_binary, 25, 255, THRESH_BINARY);  //binary frame after removing bg 
+
     // Uncomment for finding queue density
     // imshow(window_name2, img3_binary);
-    // get_frac(img3_binary);
-
+    get_frac(img3_binary,0);  //calculating density
     // Below is the part for calculating Dynamic density 
-    Mat flow(prvs.size(), CV_32FC2);
-    calcOpticalFlowFarneback(prvs, gray_frame, flow, 0.5, 3, 15, 3, 5, 1.2, 0);
 
+    Mat flow(prvs.size(), CV_32FC2);
+
+    calcOpticalFlowFarneback(prvs, gray_frame, flow, 0.5, 3, 15, 3, 5, 1.2, 0);
     Mat flow_parts[2];
     split(flow, flow_parts);
     Mat magnitude, angle, magn_norm;
@@ -122,27 +142,28 @@ int main(int argc, char* argv[])
     merge(_hsv, 3, hsv);
     hsv.convertTo(hsv8, CV_8U, 255.0);
     cvtColor(hsv8, bgr, COLOR_HSV2BGR);
-    
     cvtColor(bgr,flow,COLOR_BGR2GRAY);
     Mat flow_binary;
     threshold(flow, flow_binary, 10, 255, THRESH_BINARY);
-    imshow(window_name2, flow_binary);
-    get_frac(flow_binary);
 
+    //imshow(window_name2, flow_binary);
+
+    get_frac(flow_binary,1); //calculating density
     gray_frame.copyTo(prvs);
-    
     if (waitKey(10) == 27)
     {
       cout << "Esc key is pressed by user. Stopping the video" << endl;
       break;
     }
-  }
+  }  
+
   long double tot = (bg_changed.rows)*(bg_changed.cols)*3*255;
   ofstream answer;
-  answer.open("output.txt");
-  for(int i=0;i<aa.size();i++) answer<<aa[i]/tot<<"\n";
+  answer.open("out.txt");
+  //below part stores output in out.txt
+  answer<<"frame_no,queue_density,dynamic_density\n";
+  for(int i=0;i<aa.size();i++) answer<<5*i+1<<","<<aa[i]/tot<<","<<bb[i]/tot<<"\n";
   answer.close();
 
   return 0;
-
 }
