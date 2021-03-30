6f5835d6f0c623b576dd84006aa67568213e25f2
guptaaniket261 <guptaaniket261@gmail.com>
2021-03-31 01:31:23 +0530

    redundant part removed

--- dynamic_sparse.cpp
+++ dynamic_sparse.cpp
@@ -14,8 +14,6 @@ Rect region;
 Mat gray_frame;
 Mat frame;
 Mat prvs_frame;
-Mat toShow, mask;
-vector<Scalar> colors;
 vector<Point2f> corner_points_prev, corner_points_next;
 vector<long double> aa;
 
@@ -25,16 +23,12 @@ void findSparse()
 	vector<uchar> status;
 	vector<float> error;
 	calcOpticalFlowPyrLK(prvs_frame, gray_frame, corner_points_prev, corner_points_next, status, error, Size(15, 15), 2, criteria);
-	vector<Point2f> forNextIter;
 	int xyz = corner_points_prev.size();
 	long double s = 0;
 	for (int i = 0; i < xyz; i++)
 	{
 		if (status[i] == 1)
 		{
-			forNextIter.push_back(corner_points_next[i]);
-			line(mask, corner_points_prev[i], corner_points_next[i], colors[i], 2);
-			circle(gray_frame, corner_points_next[i], 5, colors[i], -1);
 			Point2f p1, p2;
 			p1 = corner_points_prev[i];
 			p2 = corner_points_next[i];
@@ -43,7 +37,6 @@ void findSparse()
 		}
 	}
 	aa.push_back(s);
-	add(gray_frame, mask, toShow);
 }
 
 int main(int argc, char *argv[])
@@ -78,16 +71,7 @@ int main(int argc, char *argv[])
 	warpPerspective(prvs_frame, prvs_frame, H, prvs_frame.size()); //angle corrected
 	prvs_frame = prvs_frame(region);							   //cropped background
 	int count = 0;
-	RNG rng;
-	for (int i = 0; i < 100; i++)
-	{
-		int r = rng.uniform(0, 256);
-		int g = rng.uniform(0, 256);
-		int b = rng.uniform(0, 256);
-		colors.push_back(Scalar(r, g, b));
-	}
-
-	mask = Mat::zeros(prvs_frame.size(), prvs_frame.type());
+	
 	goodFeaturesToTrack(prvs_frame, corner_points_prev, 100, 0.05, 100);
 
 	while (true)
@@ -113,7 +97,6 @@ int main(int argc, char *argv[])
 		count++;
 
 		corner_points_prev.clear();
-		mask = Mat::zeros(prvs_frame.size(), prvs_frame.type());
 		goodFeaturesToTrack(prvs_frame, corner_points_prev, 100, 0.05, 100);
 		if (waitKey(10) == 27)
 		{
